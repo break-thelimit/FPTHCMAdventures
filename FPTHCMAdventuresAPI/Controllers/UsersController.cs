5a3201a36ccb6f26321270fa4ac2a836c098fdbd
AnhKiet <97236226+Blackan06@users.noreply.github.com>
2023-07-10 11:23:05 +0700

    Update UsersController.cs

--- FPTHCMAdventuresAPI/Controllers/UsersController.cs
+++ FPTHCMAdventuresAPI/Controllers/UsersController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using AutoMapper;
 using DataAccess.Dtos.TaskItemDto;
 using Microsoft.AspNetCore.Http;
@@ -9,18 +8,7 @@ using System.Threading.Tasks;
 using System;
 using Service.Services.UserService;
 using DataAccess.Dtos.UserDto;
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using BusinessObjects.Model;
-using DataAccess.Repositories;
-using Microsoft.Extensions.Configuration;
->>>>>>> main
+
 
 namespace FPTHCMAdventuresAPI.Controllers
 {
@@ -28,7 +16,6 @@ namespace FPTHCMAdventuresAPI.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-<<<<<<< HEAD
         private readonly IUserService _userService;
         private readonly IMapper _mapper;
 
@@ -98,83 +85,3 @@ namespace FPTHCMAdventuresAPI.Controllers
         }
     }
 }
-=======
-        IUserRepository userRepository = new UserRepository();
-        private readonly IConfiguration Configuration;
-
-        public UsersController(IConfiguration config)
-        {
-            Configuration = config;
-        }
-
-        // GET: api/Users
-        [HttpGet]
-        public JsonResult GetUsers()
-        {
-            IEnumerable<User> list = userRepository.GetUsers();
-            return new JsonResult(new
-            {
-                result = list
-            });
-        }
-
-        // GET: api/Users/5
-        [HttpGet("getuser/{userid}")]
-        public JsonResult GetUser(Guid id)
-        {
-            User user = userRepository.GetUserById(id);
-            return new JsonResult(new
-            {
-                result = user
-            });
-        }
-
-        // PUT: api/Users/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut]
-        public JsonResult PutUser([FromForm] User user)
-        {
-           User tmp = userRepository.UpdateUser(user);
-            if (tmp != null)
-            {
-                return new JsonResult(new
-                {
-                    status = 200, // updated successfully!
-                    user_id = tmp.Id,
-                    message = "Updated successfully!"
-                });
-            }
-            else
-            {
-                return new JsonResult(new
-                {
-                    status = 406, // updated failed with invalid input value!
-                    message = "This user hasn't existed in system!"
-                });
-            }
-        }
-
-        // POST: api/Users
-        [HttpPost("register")]
-        public JsonResult Register(string role, [FromBody] User user)
-        {
-            bool check = userRepository.CreateAccount(role,user);
-            if (check)
-            {
-                return new JsonResult(new
-                {
-                    status = 200
-                });
-            }
-            else
-            {
-                return new JsonResult(new
-                {
-                    status = 401,
-                    message = "Sorry, this email has existed in our system!"
-                });
-            }
-        }
-    }
-}
->>>>>>> main
